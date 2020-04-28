5a21fa3108357a5707c726ad8a98224529408511
Hayden McFarland <mcfarlandsms@gmail.com>
2020-04-27 22:22:22 -0700

    refactor

--- chronos/gitlab.rb
+++ chronos/gitlab.rb
@@ -2,6 +2,8 @@
 
 module Chronos
   module Gitlab
+    module_function
+
     def load_credentials
       endpoint = "https://#{Configuration.gitlab_api_base_url}"
       ::Gitlab.endpoint = endpoint
